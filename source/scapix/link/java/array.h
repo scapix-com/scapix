37fabcd383f33162a7a7354658ff08b016c88e9e
Boris Rasin <boris@pointx.org>
2019-06-25 17:15:21 +0300

    implement full random_access_iterator

--- source/scapix/link/java/array.h
+++ source/scapix/link/java/array.h
@@ -7,6 +7,7 @@
 #ifndef SCAPIX_LINK_JAVA_ARRAY_H
 #define SCAPIX_LINK_JAVA_ARRAY_H
 
+#include <utility>
 #include <stdexcept>
 #include <scapix/link/java/object.h>
 #include <scapix/link/java/ref.h>
@@ -52,18 +53,19 @@ class array<T, std::enable_if_t<!is_primitive_v<T>>> : public array_base<T>
 
 public:
 
-	typedef ref<T> value_type;
-	typedef typename ref<T>::element_type element_type; // fix names (T could be ClassName)
-	typedef jsize size_type;
-	typedef jsize difference_type;
-	//	typedef value_type* pointer;
-	//	typedef const value_type* const_pointer;
-	//	typedef value_type& reference;
-	//	typedef const value_type& const_reference;
-	//	typedef value_type* iterator;
-	//	typedef const value_type* const_iterator;
-	//	typedef std::reverse_iterator<iterator> reverse_iterator;
-	//	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+	class reference;
+	class const_reference;
+	class iterator;
+	class const_iterator;
+
+	using value_type = ref<T>;
+	using element_type = typename ref<T>::element_type;
+	using size_type = jsize;
+	using difference_type = jsize;
+	using pointer = void;
+	using const_pointer = void;
+	using reverse_iterator = std::reverse_iterator<iterator>;
+	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 	class reference
 	{
@@ -72,18 +74,19 @@ public:
 		// We rely on C++17 guaranteed copy elision.
 		// The same could be done in C++11 with Copy-List-Initialization (return {handle, size}).
 
+		reference() = delete;
 		reference(const reference&) = delete;
 		reference(reference&&) = delete;
 
-		reference& operator = (const reference& other) = delete;
-		reference& operator = (reference&& other) { arr.set_element(pos, std::move(other).get()); return *this; }
+		void operator &() = delete;
 
-		void operator&() = delete;
+		reference& operator =(const reference& other) { *this = other.get(); return *this; }
+		reference& operator =(reference&& other) { *this = other.get(); return *this; }
 
 		template <typename Y>
-		reference& operator=(ref<Y> value) { arr.set_element(pos, value); return *this; }
+		reference& operator =(ref<Y> value) { arr.set_element(pos, value); return *this; }
 
-		operator ref<T>() const { return arr.get_element(pos); }
+		operator ref<T>() const { return get(); }
 		ref<T> get() const { return arr.get_element(pos); }
 		ref<T> operator ->() const { return get(); }
 
@@ -103,14 +106,48 @@ public:
 	{
 	public:
 
+		using difference_type = jsize;
+		using value_type = ref<T>;
+		using pointer = void;
+		using reference = reference;
+		using iterator_category = std::random_access_iterator_tag;
+
+		iterator() : pos(0) {}
 		iterator(const iterator&) = default;
+		iterator& operator =(const iterator&) = default;
 
-		iterator& operator ++() { pos++; return *this; }
-		reference operator *() { return reference(arr.handle(), pos); }
-		ref<T> operator ->() { return reference(arr.handle(), pos); }
+		iterator& operator ++() { ++pos; return *this; }
+		iterator& operator --() { --pos; return *this; }
 
-		bool operator == (const iterator& other) const { return pos == other.pos; }
-		bool operator != (const iterator& other) const { return pos != other.pos; }
+		iterator operator ++(int) { iterator temp(*this); ++pos; return temp; }
+		iterator operator --(int) { iterator temp(*this); --pos; return temp; }
+
+		iterator& operator +=(difference_type d) { pos += d; return *this; }
+		iterator& operator -=(difference_type d) { pos -= d; return *this; }
+
+		friend iterator operator + (const iterator& i, difference_type n) { iterator temp(i); i += n; return temp; }
+		friend iterator operator + (difference_type n, const iterator& i) { iterator temp(i); i += n; return temp; }
+		friend iterator operator - (const iterator& i, difference_type n) { iterator temp(i); i -= n; return temp; }
+		friend iterator operator - (difference_type n, const iterator& i) { iterator temp(i); i -= n; return temp; }
+
+		reference operator [](difference_type n) const { return *(*this + n); }
+
+		reference operator *() const { return reference(arr.handle(), pos); }
+		reference operator ->() const { return reference(arr.handle(), pos); }
+
+		friend bool operator == (const iterator& a, const iterator& b) { return a.pos == b.pos; }
+		friend bool operator != (const iterator& a, const iterator& b) { return a.pos != b.pos; }
+		friend bool operator >  (const iterator& a, const iterator& b) { return a.pos >  b.pos; }
+		friend bool operator >= (const iterator& a, const iterator& b) { return a.pos >= b.pos; }
+		friend bool operator <  (const iterator& a, const iterator& b) { return a.pos < b.pos; }
+		friend bool operator <= (const iterator& a, const iterator& b) { return a.pos <= b.pos; }
+
+		friend void swap(iterator& a, iterator& b)
+		{
+			using std::swap;
+			swap(a.arr, b.arr);
+			swap(a.pos, b.pos);
+		}
 
 	private:
 
@@ -127,13 +164,23 @@ public:
 	{
 	public:
 
-		const_reference(const const_reference&) = default;
+		// We rely on C++17 guaranteed copy elision.
+		// The same could be done in C++11 with Copy-List-Initialization (return {handle, size}).
 
-		void operator&() = delete;
+		const_reference() = delete;
+		const_reference(const const_reference&) = delete;
+		const_reference(const_reference&&) = delete;
 
-		operator ref<T>() const { return arr.get_element(pos); }
+		void operator &() = delete;
+
+		operator ref<T>() const { return get(); }
+		ref<T> get() const { return arr.get_element(pos); }
+		ref<T> operator ->() const { return get(); }
 
 	private:
+
+		friend array;
+		friend class iterator;
 
 		const_reference(typename array::handle_type arr, jsize pos) : arr(arr), pos(pos) {}
 
@@ -142,23 +189,81 @@ public:
 
 	};
 
+	class const_iterator
+	{
+	public:
+
+		using difference_type = jsize;
+		using value_type = ref<T>;
+		using pointer = void;
+		using reference = const_reference;
+		using iterator_category = std::random_access_iterator_tag;
+
+		const_iterator() : pos(0) {}
+		const_iterator(const const_iterator&) = default;
+		const_iterator& operator =(const const_iterator&) = default;
+
+		const_iterator& operator ++() { ++pos; return *this; }
+		const_iterator& operator --() { --pos; return *this; }
+
+		const_iterator operator ++(int) { const_iterator temp(*this); ++pos; return temp; }
+		const_iterator operator --(int) { const_iterator temp(*this); --pos; return temp; }
+
+		const_iterator& operator +=(difference_type d) { pos += d; return *this; }
+		const_iterator& operator -=(difference_type d) { pos -= d; return *this; }
+
+		friend const_iterator operator + (const const_iterator& i, difference_type n) { const_iterator temp(i); i += n; return temp; }
+		friend const_iterator operator + (difference_type n, const const_iterator& i) { const_iterator temp(i); i += n; return temp; }
+		friend const_iterator operator - (const const_iterator& i, difference_type n) { const_iterator temp(i); i -= n; return temp; }
+		friend const_iterator operator - (difference_type n, const const_iterator& i) { const_iterator temp(i); i -= n; return temp; }
+
+		reference operator [](difference_type n) const { return *(*this + n); }
+
+		reference operator *() const { return reference(arr.handle(), pos); }
+		reference operator ->() const { return reference(arr.handle(), pos); }
+
+		friend bool operator == (const const_iterator& a, const const_iterator& b) { return a.pos == b.pos; }
+		friend bool operator != (const const_iterator& a, const const_iterator& b) { return a.pos != b.pos; }
+		friend bool operator >  (const const_iterator& a, const const_iterator& b) { return a.pos > b.pos; }
+		friend bool operator >= (const const_iterator& a, const const_iterator& b) { return a.pos >= b.pos; }
+		friend bool operator <  (const const_iterator& a, const const_iterator& b) { return a.pos < b.pos; }
+		friend bool operator <= (const const_iterator& a, const const_iterator& b) { return a.pos <= b.pos; }
+
+		friend void swap(const_iterator& a, const_iterator& b)
+		{
+			using std::swap;
+			swap(a.arr, b.arr);
+			swap(a.pos, b.pos);
+		}
+
+	private:
+
+		friend array;
+
+		const_iterator(typename array::handle_type arr, jsize pos) : arr(arr), pos(pos) {}
+
+		array arr;
+		jsize pos;
+
+	};
+
 	bool empty() const { return this->size() == 0; }
 
-	iterator begin() { return iterator(this->handle(),  0); }
-	//const_iterator begin() const { return data(); }
-	//const_iterator cbegin() const { return data(); }
+	iterator begin() { return iterator(this->handle(), 0); }
+	const_iterator begin() const { return const_iterator(this->handle(), 0); }
+	const_iterator cbegin() const { return begin(); }
 
-	iterator end() { return iterator(this->handle(),  this->size()); }
-	//const_iterator end() const { return data() + size(); }
-	//const_iterator cend() const { return data() + size(); }
+	iterator end() { return iterator(this->handle(), this->size()); }
+	const_iterator end() const { return const_iterator(this->handle(), this->size()); }
+	const_iterator cend() const { return end(); }
 
-	//reverse_iterator rbegin() { return data() + size();	}
-	//const_reverse_iterator rbegin() const { return data() + size();	}
-	//const_reverse_iterator crbegin() const { return data() + size(); }
+	reverse_iterator rbegin() { return reverse_iterator(end()); }
+	const_reverse_iterator rbegin() const { return const_reverse_iterator(end());	}
+	const_reverse_iterator crbegin() const { return rbegin(); }
 
-	//reverse_iterator rend() { return data(); }
-	//const_reverse_iterator rend() const { return data(); }
-	//const_reverse_iterator crend() const { return data(); }
+	reverse_iterator rend() { return reverse_iterator(begin()); }
+	const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+	const_reverse_iterator crend() const { return rend(); }
 
 	reference operator[](size_type pos) { return reference(this->handle(), pos); }
 	const_reference operator[](size_type pos) const { return const_reference(this->handle(), pos); }
