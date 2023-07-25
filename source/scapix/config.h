9e13e4c242a785d8ea1e87413c304d26cda83e7b
Boris Rasin <boris@pointx.org>
2023-07-26 01:08:08 +0300

    refactor: remove unused option SCAPIX_PLATFORM

--- source/scapix/config.h
+++ source/scapix/config.h
@@ -9,14 +9,7 @@
 
 #define SCAPIX_INCLUDE(a,b,c) <a/b/c>
 
-#define SCAPIX_PLATFORM_INCLUDE(file) SCAPIX_INCLUDE(scapix/platform, SCAPIX_PLATFORM, file)
 #define SCAPIX_BRIDGE_INCLUDE(file) SCAPIX_INCLUDE(scapix/bridge, SCAPIX_BRIDGE, file)
-
-#ifndef SCAPIX_PLATFORM
-#define SCAPIX_PLATFORM generic
-#endif
-
-// SCAPIX_BRIDGE: cpp, java, objc, python
 
 #ifndef SCAPIX_BRIDGE
 #define SCAPIX_BRIDGE cpp
