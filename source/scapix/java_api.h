0b57d16aa9e803168c5ae64ccd4cad84a076a08a
Boris Rasin <boris@pointx.org>
2024-02-28 15:54:42 +0200

    feat(java): support for java_api 2.0 generated headers

--- source/scapix/java_api.h
+++ source/scapix/java_api.h
@@ -8,13 +8,23 @@
 #define SCAPIX_JAVA_API_H
 
 #include <scapix/jni/object_base.h>
+#include <scapix/jni/ref.h>
+#include <scapix/jni/array.h>
 #include <scapix/jni/convert.h>
 
 namespace scapix::java_api {
 
+// compatibility with scapix_java 1.0 generated headers
+
+#define SCAPIX_META_STRING(x) x
 using jni::object_base;
 using jni::ref;
-using jni::generic;
+namespace link::java { using jni::array; }
+
+// workaround for conflict between namespaces "sun" and "com::sun"
+
+namespace sun {}
+namespace com::sun { namespace sun = ::scapix::java_api::sun; }
 
 } // namespace scapix::java_api
 
